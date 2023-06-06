323701c4d98bd6b8fd3a18ee871f3d13930ef56d
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-06-06 12:02:57 -0700

    finished assginment

---
+++ TestProjects/testmath/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace testmath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program”");
+            string usernumber = Console.ReadLine();
+            int usernumbers = Convert.ToInt32(usernumber); //this converts the string to int
+            Console.WriteLine(usernumbers * 50);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ myConsoleProject.cs/testmath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		
+		
+		Console.WriteLine("input a number");
+		string usernumber = Console.ReadLine();
+		int usernumbers = Convert.ToInt32(usernumber); //this converts the string to int
+		Console.WriteLine(usernumbers * 50);
+		
+		
+	}
+}
