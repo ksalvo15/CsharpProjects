ba018c68420df32ae497c7fbc211c4b656d81301
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-06-07 13:23:52 -0700

    finished bool project

---
+++ Boolean Logic Assign/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace testmath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("car insurance");
+
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int ofAge = Convert.ToInt32(age); //this converts the string to int
+            bool oldenough = ofAge > 15;
+
+            Console.WriteLine("Have you ever had a DUI true or false?");
+            bool DUI; //needs to have this variable for the if statement
+            bool noDUI = true; //this takes the if statment noDUI and makes it a bool statement
+            DUI = Convert.ToBoolean(Console.ReadLine()); //this converts the input to a bool
+            if (DUI == false)
+            {
+                noDUI = true;
+            }
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            int ticket = Convert.ToInt32(tickets); //this converts the string to int
+            bool nottoomanytickets = ticket < 3;
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(oldenough && noDUI && nottoomanytickets); //this takes the variables oldenough noDUI and toomanytickets and sees if they are all true and prints out if they are qualified
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
--- Operator Submission Assignment/ConsoleApp1/Program.cs
+++ Operator Submission Assignment/ConsoleApp1/Program.cs
@@ -7,24 +7,31 @@ namespace testmath
         static void Main(string[] args)
         {
             Console.WriteLine("Anonymous Income Comparison Program");
+            
             Console.WriteLine("Person 1 input hourly rate");
             string Person1Hourly = Console.ReadLine();
             int Person1Hourlys = Convert.ToInt32(Person1Hourly); //this converts the string to int
+            
             Console.WriteLine("Person 1 input hours worked");
             string Person1Hourworked = Console.ReadLine();
             int Person1Hoursworked = Convert.ToInt32(Person1Hourworked); //this converts the string to int
+            
             Console.WriteLine("Person 2 input hourly rate");
             string Person2Hourly = Console.ReadLine();
             int Person2Hourlys = Convert.ToInt32(Person2Hourly); //this converts the string to int
+            
             Console.WriteLine("Person 2 input hours worked");
             string Person2Hourworked = Console.ReadLine();
             int Person2Hoursworked = Convert.ToInt32(Person2Hourworked); //this converts the string to int
+            
             Console.WriteLine("Annual salary of Person 1:");
             int Person1Annual = Person1Hoursworked * Person1Hourlys;
             Console.WriteLine((Person1Hoursworked * Person1Hourlys * 52).ToString("C")); //converts the salary to money vs int
+           
             Console.WriteLine("Annual salary of Person 2:");
             int Person2Annual = Person2Hoursworked * Person2Hourlys;
-            Console.WriteLine((Person2Hoursworked * Person2Hourlys * 52m).ToString("C")); //converts the salary to money vs int
+            Console.WriteLine((Person2Hoursworked * Person2Hourlys * 52).ToString("C")); //converts the salary to money vs int
+            
             Console.WriteLine("Does Person 1 make more money than Person 2?");
             bool Person1Greater = Person1Annual > Person2Annual;
             Console.WriteLine(Person1Greater);
