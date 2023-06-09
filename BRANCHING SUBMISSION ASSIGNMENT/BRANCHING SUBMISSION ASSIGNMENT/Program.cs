721ae4217d29e725eb5fa5ac9781d8c77ec048e2
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-06-09 10:15:01 -0700

    finished assignment

---
+++ BRANCHING SUBMISSION ASSIGNMENT/BRANCHING SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace testmath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("What is the weight of your package?");
+            string weight = Console.ReadLine();
+            int numweight = Convert.ToInt32(weight);
+
+            if (numweight > 50)
+                { 
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                }
+
+            else
+                {
+                Console.WriteLine("What is the package height?");
+                string height = Console.ReadLine();
+                int packheight = Convert.ToInt32(height);
+
+                Console.WriteLine("What is the package length?");
+                string length = Console.ReadLine();
+                int packlength = Convert.ToInt32(length);
+
+                Console.WriteLine("What is the package width?");
+                string width = Console.ReadLine();
+                int packwidth = Convert.ToInt32(width);
+
+                int TotalPrice = ((packwidth * packheight * packlength * numweight) / 100);
+                string moneyprice = TotalPrice.ToString("C");
+
+                Console.WriteLine("Your estimated total for shipping this package is: " + moneyprice);
+                Console.ReadLine();
+                }
+            
+            
+
+
+        }
+    }
+}
