3210649fb9808907c84b71ade665eb2083d6f6b6
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-06-12 12:18:23 -0700

    created while loop

---
+++ Console App/CONSOLE APP/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CONSOLE_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a color");
+            string color = Console.ReadLine();
+            bool picked = false;
+
+            do
+            {
+                switch (color)
+                {
+                    case "pink":
+                        Console.WriteLine("you picked " + color + ", fun fact about " + color + ", the pink power rangers name is Kimberly");
+                        Console.WriteLine("pick another color");
+                        color = Console.ReadLine();
+                        break;
+                    case "blue":
+                        Console.WriteLine("you picked " + color + ", fun fact about " + color + " The ocean is blue because water absorbs colors in the red part of the light spectrum");
+                        Console.WriteLine("pick another color");
+                        color = Console.ReadLine();
+                        break;
+                    case "green":
+                        Console.WriteLine("you picked " + color + ", fun fact about " + color + " green is the best color for photosynthesis for plants");
+                        picked = true;
+                        Console.WriteLine("thats also my favorite color!");
+                        Console.WriteLine("pick another color");
+                        color = Console.ReadLine();
+                        break;
+                    case "purple":
+                        Console.WriteLine("you picked " + color + ", fun fact about " + color + " Carrots used to be purple, now most are orange!");
+                        Console.WriteLine("pick another color");
+                        color = Console.ReadLine();
+                        break;
+                    case "red":
+                        Console.WriteLine("you picked " + color + ", fun fact about " + color + " Red is the first color babies can see after black and white");
+                        Console.WriteLine("pick another color");
+                        color = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("you picked a color that I don't have a fun fact about");
+                        Console.WriteLine("pick another color");
+                        color = Console.ReadLine();
+                        break;
+                }
+            }
+
+            while (!picked);
+
+            Console.ReadLine();
+        }
+    }
+}
