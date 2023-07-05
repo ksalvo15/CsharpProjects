a5a4046dce1dd1254419811fb14e6fb5802c05c1
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-07-05 13:58:37 -0700

    edits

--- MAIN METHOD/MAIN METHOD/Program.cs
+++ MAIN METHOD/MAIN METHOD/Program.cs
@@ -14,6 +14,7 @@ namespace MAIN_METHOD
 
             Console.WriteLine(intnum + " * 2 = " + result1);
 
+
             //Part 2
             Console.WriteLine("What decimal do you want to use to do math operations?");
             decimal decnum = Convert.ToDecimal(Console.ReadLine());            
@@ -21,18 +22,17 @@ namespace MAIN_METHOD
 
             Console.WriteLine(decnum + " + 1 = " + result2);
 
+
             //Part3
             Console.WriteLine("What string do you want to use to do math operations?");
             try
-            {
-                
+            {                
                 string stringnumber = Console.ReadLine();
 
                 int result3 = submethod.Math(stringnumber);
 
-                Console.WriteLine(stringnumber + " converted to a number + 1 is = " + result3); //this seems to be multiplying it by 2?
+                Console.WriteLine(stringnumber + " converted to a number + 1 is = " + result3);
             }
-
             catch (FormatException)
             {
                 Console.WriteLine("the string entered could not be converted to an integer");
--- MAIN METHOD/MAIN METHOD/submethod.cs
+++ MAIN METHOD/MAIN METHOD/submethod.cs
@@ -10,21 +10,20 @@ namespace MAIN_METHOD
         {
             int result1 = intnum * 2;
             return result1;
-
         }
+
 
         public static int Math(decimal decnum)
         {            
             int result2 = (int)(decnum + 1);
             return result2;
-
         }
         
+
         public static int Math(string stringnumber)
         {            
             int result3 = Convert.ToInt32(stringnumber) + 1;
             return result3;
-
         }
     }
 }
