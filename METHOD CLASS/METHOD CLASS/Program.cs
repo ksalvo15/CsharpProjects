6a04a3b1efd4863f198c5d651e61b559612008c4
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-07-06 10:48:11 -0700

    void class math

---
+++ METHOD CLASS/METHOD CLASS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace METHOD_CLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            Voidclass math = new Voidclass();
+
+            //part3
+            Console.WriteLine("please add an integer");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("please add another integer");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            //part1 2 and 4
+            math.Math(var1: 5, var2: 7, input1, input2); //this calls the method in the class with the parameters by name as var1= 5 and var 2 = 7
+
+        }
+    }
+}
---
+++ METHOD CLASS/METHOD CLASS/Voidclass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace METHOD_CLASS
+{
+    public class Voidclass
+    {
+        public void Math(int var1, int var2, int input1, int input2)
+        {
+            //Part1 and 2
+            int result = var1 + 3;
+            Console.WriteLine(var1 + " + 3 = " + result);
+            Console.WriteLine("this number is just being displayed" + var2);
+
+            Console.ReadLine();
+
+            //Part3
+            int result2 = input1 * input2;
+            Console.WriteLine(input1 + " * " + input2 + " = " + result2);
+        }
+    }
+}
