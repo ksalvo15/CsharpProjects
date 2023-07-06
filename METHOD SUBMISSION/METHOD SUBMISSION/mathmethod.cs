b062855557b79c891f643d68fc5e8265b8f6b95e
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-07-06 10:08:56 -0700

    edits

--- METHOD SUBMISSION/METHOD SUBMISSION/mathmethod.cs
+++ METHOD SUBMISSION/METHOD SUBMISSION/mathmethod.cs
@@ -6,7 +6,7 @@ namespace METHOD_SUBMISSION
 {
     public class mathmethod
     {
-        public int Math(int var1, int var2 = 0)
+        public int Math(int var1, int var2 = 0) //making it 0 lets the second nubmer be optional? ()
         {            
             int result = var1 + var2;
             return result;
