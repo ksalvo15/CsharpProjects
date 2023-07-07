9a1c5e28c1820739f497bb6283d45a47793a33b4
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-07-07 10:17:43 -0700

    minor edits

--- METHOD CLASS/METHOD CLASS/Program.cs
+++ METHOD CLASS/METHOD CLASS/Program.cs
@@ -13,13 +13,16 @@ namespace METHOD_CLASS
             Console.WriteLine("please add an integer");
             int input1 = Convert.ToInt32(Console.ReadLine());
 
-
             Console.WriteLine("please add another integer");
             int input2 = Convert.ToInt32(Console.ReadLine());
 
-            //part1 2 and 4
-            math.Math(var1: input1, var2: input2); //this calls the method in the class with the parameters by name as var1= 5 and var 2 = 7
+            //part3
+            math.Math(input1, input2);
 
+            //part 4
+            math.Math(var1: input1, var2: input2); //this calls the method in the class with the parameters by name as var1= 5 and var 2 = 7
+            
+            
         }
     }
 }
