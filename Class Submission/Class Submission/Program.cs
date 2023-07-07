96a0ed508aabb8a651e08fb7eacddbea94bab9cc
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-07-07 12:24:32 -0700

    Class submission edits

--- Class Submission/Class Submission/Program.cs
+++ Class Submission/Class Submission/Program.cs
@@ -4,9 +4,25 @@ namespace Class_Submission
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            VoidMethod math = new VoidMethod();
+
+            //part3
+            Console.WriteLine("please add an integer");
+            int result;
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            
+            math.Math(num1, out result);
+
+            //part 5
+            Console.WriteLine("please add a string");
+            string var2 = Console.ReadLine();
+            math.Math(var2);
+
+            //Part6
+            VoidMethod.StaticM();
+
         }
     }
 }
---
+++ Class Submission/Class Submission/VoidMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Submission
+{
+    public class VoidMethod
+    {       
+        public void Math(int var1, out int result) //part 2 output parameters
+        {
+            //Part1
+            result = var1 / 2;
+            Console.WriteLine(var1 + " / 2 = " + result);
+                
+            Console.ReadLine();
+        }
+
+        //part 5 overloading the method
+        public void Math(string var1)
+        {
+            //Part1
+            string result = var1;
+            Console.WriteLine(var1 + " strings cannot be divided by 2");
+
+            Console.ReadLine();
+        }
+
+        public static void StaticM()
+        {
+            //Part6
+            Console.WriteLine("static method?");
+
+        }
+    }
+}
