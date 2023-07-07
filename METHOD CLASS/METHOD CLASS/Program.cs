73745884cab5fe41ac799949504f9ab506876244
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-07-07 10:05:36 -0700

    edits

--- METHOD CLASS/METHOD CLASS/Program.cs
+++ METHOD CLASS/METHOD CLASS/Program.cs
@@ -18,7 +18,7 @@ namespace METHOD_CLASS
             int input2 = Convert.ToInt32(Console.ReadLine());
 
             //part1 2 and 4
-            math.Math(var1: 5, var2: 7, input1, input2); //this calls the method in the class with the parameters by name as var1= 5 and var 2 = 7
+            math.Math(var1: input1, var2: input2); //this calls the method in the class with the parameters by name as var1= 5 and var 2 = 7
 
         }
     }
--- METHOD CLASS/METHOD CLASS/Voidclass.cs
+++ METHOD CLASS/METHOD CLASS/Voidclass.cs
@@ -6,18 +6,16 @@ namespace METHOD_CLASS
 {
     public class Voidclass
     {
-        public void Math(int var1, int var2, int input1, int input2)
+        public void Math(int var1, int var2)
         {
             //Part1 and 2
             int result = var1 + 3;
             Console.WriteLine(var1 + " + 3 = " + result);
-            Console.WriteLine("this number is just being displayed" + var2);
+            Console.WriteLine(var2 + " this number is just being displayed");
 
             Console.ReadLine();
 
-            //Part3
-            int result2 = input1 * input2;
-            Console.WriteLine(input1 + " * " + input2 + " = " + result2);
+            
         }
     }
 }
