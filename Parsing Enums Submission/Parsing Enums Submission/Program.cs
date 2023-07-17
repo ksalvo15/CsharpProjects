7fad5c0bbb9a6debe7d32124fa536c08c32463fa
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-07-17 09:35:44 -0700

    edits

--- Parsing Enums Submission/Parsing Enums Submission/Program.cs
+++ Parsing Enums Submission/Parsing Enums Submission/Program.cs
@@ -6,38 +6,26 @@ namespace Parsing_Enums_Submission
     {
         public enum DayofWeek
         {
-            monday = 1,
-            tuesday = 2,
-            wednesday = 3,
-            thursday = 4,
-            friday = 5,
-            saturday = 6,
-            sunday = 7
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+            sunday
 
         }
         public static void Main(string[] args)
         {
-            
-
             try
             {
                 Console.WriteLine("Please enter a day of the week");
                 string userday = Console.ReadLine();
-                DayofWeek day;
-                //for (int i = 0; i < 7; i++)
-                //{
-                    if (Enum.TryParse(userday, true, out day))
-                    {
-
-                        Console.WriteLine("you wrote one of the days of the week {0}", day);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Please enter an actual day of the week");
-                    }
-                //}                
+                DayofWeek day = (DayofWeek)Enum.Parse(typeof(DayofWeek), userday);
+                Console.WriteLine("you wrote a day of the week {0}", day);
+                
             }
-            catch (FormatException)
+            catch (ArgumentException)
             {
                 Console.WriteLine("Please enter a string");
             }
