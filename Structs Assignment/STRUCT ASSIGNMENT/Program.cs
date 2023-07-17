9a672fb5829de1c00bb08919fe93db2833528853
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-07-17 11:05:15 -0700

    edits

---
+++ Structs Assignment/STRUCT ASSIGNMENT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace STRUCT_ASSIGNMENT
+{
+
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Number myNumber = new Number();
+
+            myNumber.Amount = 154.52m;
+
+        Console.WriteLine("here is the amount {0}", myNumber.Amount);
+        }
+    }
+}
