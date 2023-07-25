196cf4a2a2672512d01a95b507756f8cc29625ba
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2023-07-25 11:56:59 -0700

    datetime

--- BlackJack/BlackJack/Dealer.cs
+++ BlackJack/BlackJack/Dealer.cs
@@ -20,6 +20,7 @@ namespace BlackJack
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\kelse\Desktop\Logs\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
                 Deck.Cards.RemoveAt(0);
---
+++ Datetime Assignment/DateTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please write a number");
+            int usernumber = Convert.ToInt32(Console.ReadLine());
+            DateTime futureTime = currentDateTime.AddHours(usernumber); //this adds the number that was entered in by the user to the current date time
+            Console.WriteLine("you wrote {0}, it will be {1} in {0}hours", usernumber, futureTime);
+        }
+    }
+}
